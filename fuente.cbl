3ab7cc6bc846fa567e1a70c5fdccda37da2bd80c
unknown <bolch3@gmail.com>
2020-03-28 20:03:42 -0300

    primera fuente cobol

---
+++ fuente.cbl
@@ -0,0 +1,131 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. INICIO.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT OPTIONAL CLIENTES ASSIGN TO "./clientes.dat"
+                  ORGANIZATION INDEXED
+                  ACCESS MODE DYNAMIC
+                  RECORD KEY IS ID_CLIENTE
+                  ALTERNATE KEY CLI_NOMBRE WITH DUPLICATES
+                  ALTERNATE KEY CLI_NOMBRE_2    WITH DUPLICATES
+                  STATUS ST-FILE.
+
+
+       DATA DIVISION.
+       FILE SECTION.
+
+       FD  CLIENTES.
+
+       01  REG-CLIENTES.
+           03  ID_CLIENTE.
+               05 CLI_ID                   PIC 9(8).
+           03 CLI_SALDO                    PIC S9(7)V9(3).
+           03 CLI_NOMBRE                   PIC X(60).
+           03 CLI_DIRECCION                PIC X(80).
+           03 CLI_CODPOSTAL                PIC X(10).
+           03 CLI_CATEGORIA                PIC X.
+           03 CLI_ALT_2.
+               05 CLI_CATEGORIA_2          PIC X.
+               05 CLI_NOMBRE_2             PIC X(60).
+           03 CLI_RAZONSOCIAL              PIC X(60).
+           03 CLI_VENTAS.
+               04 CLIVENTAS OCCURS 12 TIMES.
+                  05 CLI_TOTAL               PIC S9(9)V99.
+           03 FILLER                       PIC X(240).
+
+       WORKING-STORAGE SECTION.
+       01  ST-FILE PIC XX.
+       01  X       PIC X.
+
+       01  TOTAL-FACTURA PIC S9(6)V99.
+
+       77  BANDERA  PIC 9.
+       01  SALDO-Z  PIC Z(6)9,99.
+       01  SALDO-ZZ PIC -------9,99.
+
+       01  FECHA-AUX.
+           02 AA       PIC 9999.
+           02 MM       PIC 99.
+           02 DD       PIC 99.
+
+       01  MESES.
+           03 MES OCCURS 12 TIMES.
+               05 MES-NOMBRE   PIC X(20).
+
+       01  VARIABLES.
+           03 VALORES OCCURS 12 TIMES.
+               05 VALOR-DIA OCCURS 31 TIMES.
+                   07 VALOR-VENTA PIC S9(09)V999.
+
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            MOVE "ENERO" TO MES-NOMBRE (01).
+            MOVE "FEBRERO" TO MES-NOMBRE (02).
+            MOVE "MARZO" TO MES-NOMBRE (03).
+
+           MOVE FACT-FECHA TO FECHA-AUX.
+            COMPUTE VALOR-VENTA(MM, DD)=VALOR-VENTA(MM, DD)+
+            TOTAL-FACTURA.
+
+           ADD TOTAL-FACTURA TO VALOR-VENTA(MM, DD).
+           SUBTRACT TOTAL-FACTURA FROM VALOR-VENTA(MM, DD).
+            STOP RUN.
+       ABRO-ARCHIVOS.
+           OPEN I-O CLIENTES.
+           IF ST-FILE >"07"
+               DISPLAY "ERROR ABRIENDO EL ARCHIVO".
+
+
+       CIERRO-ARCHIVOS.
+           CLOSE CLIENTES.
+
+       GRABO-DATOS.
+           INITIALIZE REG-CLIENTES.
+           MOVE 1 TO CLI_ID.
+           MOVE 0 TO CLI_SALDO.
+           MOVE "VAR-NOMBRE" TO CLI_NOMBRE.
+           MOVE "W-DIRECCION" TO CLI_DIRECCION.
+
+
+       GRABO-REGISTRO.
+           WRITE REG-CLIENTES.
+           IF ST-FILE ="99" GO TO GRABO-DATOS.
+           IF ST-FILE > "07"
+               DISPLAY "error grabando el archivo".
+       F-GRABO-DATOS.
+           EXIT.
+
+       LEO-DATOS.
+           INITIALIZE REG-CLIENTES.
+           START CLIENTES KEY IS NOT LESS THAN ID_CLIENTE.
+           READ CLIENTES NEXT RECORD.
+           IF ST-FILE ="99" GO TO LEO-DATOS.
+           IF ST-FILE > "07"
+               DISPLAY "error leyendo el archivo".
+
+       MUESTRO-DATOS.
+           MOVE CLI_SALDO TO SALDO-Z.
+           DISPLAY "Id cliente"    LINE 10 COL 5.
+           DISPLAY "Saldo"         LINE 11 COL 5.
+           DISPLAY "Nombre"        LINE 12 COL 5.
+           DISPLAY "Direccion"     LINE 13 COL 5.
+           DISPLAY CLI_ID          LINE 10 COL 30.
+           DISPLAY  SALDO-Z        LINE 11 COL 30.
+           DISPLAY CLI_NOMBRE      LINE 12 COL 30.
+           DISPLAY CLI_DIRECCION   LINE 13 COL 30.
+           ACCEPT X LINE 14 COL 70.
+
+
+       F-LEO-DATOS.
+           EXIT.
+       END PROGRAM INICIO.

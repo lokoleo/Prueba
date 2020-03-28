1b33fb306de40a43de2df9a9279d8bb7e0c0c6b5
unknown <bolch3@gmail.com>
2020-03-28 20:25:32 -0300

    fuente final

--- fuente.cbl
+++ fuente.cbl
@@ -5,21 +5,21 @@
       * Tectonics: cobc
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. INICIO.
+       PROGRAM-ID. "INICIO".
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SPECIAL-NAMES.
            DECIMAL-POINT IS COMMA.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
+
            SELECT OPTIONAL CLIENTES ASSIGN TO "./clientes.dat"
                   ORGANIZATION INDEXED
                   ACCESS MODE DYNAMIC
                   RECORD KEY IS ID_CLIENTE
                   ALTERNATE KEY CLI_NOMBRE WITH DUPLICATES
-                  ALTERNATE KEY CLI_NOMBRE_2    WITH DUPLICATES
+                  ALTERNATE KEY CLI_ALT_2  WITH DUPLICATES
                   STATUS ST-FILE.
-
 
        DATA DIVISION.
        FILE SECTION.
@@ -28,104 +28,230 @@
 
        01  REG-CLIENTES.
            03  ID_CLIENTE.
-               05 CLI_ID                   PIC 9(8).
-           03 CLI_SALDO                    PIC S9(7)V9(3).
-           03 CLI_NOMBRE                   PIC X(60).
-           03 CLI_DIRECCION                PIC X(80).
-           03 CLI_CODPOSTAL                PIC X(10).
-           03 CLI_CATEGORIA                PIC X.
-           03 CLI_ALT_2.
-               05 CLI_CATEGORIA_2          PIC X.
-               05 CLI_NOMBRE_2             PIC X(60).
-           03 CLI_RAZONSOCIAL              PIC X(60).
-           03 CLI_VENTAS.
-               04 CLIVENTAS OCCURS 12 TIMES.
-                  05 CLI_TOTAL               PIC S9(9)V99.
-           03 FILLER                       PIC X(240).
+               05  CLI_ID           PIC 9(7).
+           03  CLI_SALDO            PIC S9(7)V9(3).
+           03  CLI_NOMBRE           PIC X(70).
+           03  CLI_DIRECCION        PIC X(80).
+           03  CLI_CODPOST          PIC X(10).
+           03  CLI_CATEGORIA        PIC X.
+           03  CLI_ALT_2.
+               05  CLI_CATEGORIA_2  PIC X.
+               05  CLI_NOMBRE_2     PIC X(60).
+           03  FILLER               PIC X(240).
+
 
        WORKING-STORAGE SECTION.
-       01  ST-FILE PIC XX.
-       01  X       PIC X.
 
-       01  TOTAL-FACTURA PIC S9(6)V99.
+       01  ST-FILE   PIC XX.
+       01  X         PIC X.
 
-       77  BANDERA  PIC 9.
-       01  SALDO-Z  PIC Z(6)9,99.
-       01  SALDO-ZZ PIC -------9,99.
+       01  MENSAJE   PIC X(70).
 
-       01  FECHA-AUX.
-           02 AA       PIC 9999.
-           02 MM       PIC 99.
-           02 DD       PIC 99.
+       01  FIN        PIC X     VALUES "N".
+       01  EXISTE     PIC X.
+       01  HUBO-ERROR PIC 9     VALUES 0.
+       01  GUIONES    PIC X(80) VALUES ALL "-".
+       01  OPCION     PIC 99.
 
-       01  MESES.
-           03 MES OCCURS 12 TIMES.
-               05 MES-NOMBRE   PIC X(20).
+       01  W-CLI-ID   PIC 9(07).
+       01  W-CLI-ID-Z PIC Z(06)9.
 
-       01  VARIABLES.
-           03 VALORES OCCURS 12 TIMES.
-               05 VALOR-DIA OCCURS 31 TIMES.
-                   07 VALOR-VENTA PIC S9(09)V999.
+       01  DATOS.
+           02 W-CLI-NOMBRE     PIC X(70).
+           02 W-CLI-NOMBRE-ANT PIC X(70).
+           02 W-CLI-DIRECCION  PIC X(80).
+           02 W-CLI-CODPOSTAL  PIC X(10).
+           02 W-CLI-CATEGORIA  PIC X.
 
        PROCEDURE DIVISION.
-       MAIN-PROCEDURE.
-            MOVE "ENERO" TO MES-NOMBRE (01).
-            MOVE "FEBRERO" TO MES-NOMBRE (02).
-            MOVE "MARZO" TO MES-NOMBRE (03).
+       PABLO.
+           PERFORM INICIALIZACION.
+           PERFORM ABRO-ARCHIVO.
+           PERFORM PROCESO THRU F-PROCESO UNTIL FIN = "S".
+           PERFORM CIERRO-ARCHIVO.
+           GO TO FINALIZAR.
 
-           MOVE FACT-FECHA TO FECHA-AUX.
-            COMPUTE VALOR-VENTA(MM, DD)=VALOR-VENTA(MM, DD)+
-            TOTAL-FACTURA.
+       INICIALIZACION.
+           MOVE "N" TO FIN.
+           MOVE "S" TO EXISTE.
 
-           ADD TOTAL-FACTURA TO VALOR-VENTA(MM, DD).
-           SUBTRACT TOTAL-FACTURA FROM VALOR-VENTA(MM, DD).
-            STOP RUN.
-       ABRO-ARCHIVOS.
+       ABRO-ARCHIVO.
            OPEN I-O CLIENTES.
-           IF ST-FILE >"07"
-               DISPLAY "ERROR ABRIENDO EL ARCHIVO".
+           IF ST-FILE > "07"
+              STRING "Error al abrir Clientes " ST-FILE
+                 DELIMITED BY SIZE INTO MENSAJE
+              DISPLAY MENSAJE LINE 10 COL 20
+              MOVE "S" TO FIN.
 
-
-       CIERRO-ARCHIVOS.
+       CIERRO-ARCHIVO.
            CLOSE CLIENTES.
 
-       GRABO-DATOS.
-           INITIALIZE REG-CLIENTES.
-           MOVE 1 TO CLI_ID.
-           MOVE 0 TO CLI_SALDO.
-           MOVE "VAR-NOMBRE" TO CLI_NOMBRE.
-           MOVE "W-DIRECCION" TO CLI_DIRECCION.
+       FINALIZAR.
+           STOP RUN.
+
+       PROCESO.
+           PERFORM MUESTRO-PANTALLA.
+           PERFORM INGRESO-ID THRU F-INGRESO-ID.
+           IF FIN = "N"
+              PERFORM LEO-CLIENTES THRU F-LEO-CLIENTES
+              IF HUBO-ERROR = 1
+                 MOVE "S" TO FIN
+                 GO TO F-PROCESO
+              END-IF
+              IF EXISTE = "S"
+                 PERFORM MUESTRO-DATOS
+              ELSE
+                 PERFORM CARGO-DATOS THRU F-CARGO-DATOS
+              END-IF
+              PERFORM OPCIONES.
 
 
-       GRABO-REGISTRO.
-           WRITE REG-CLIENTES.
-           IF ST-FILE ="99" GO TO GRABO-DATOS.
-           IF ST-FILE > "07"
-               DISPLAY "error grabando el archivo".
-       F-GRABO-DATOS.
+       F-PROCESO.
            EXIT.
 
-       LEO-DATOS.
-           INITIALIZE REG-CLIENTES.
-           START CLIENTES KEY IS NOT LESS THAN ID_CLIENTE.
-           READ CLIENTES NEXT RECORD.
-           IF ST-FILE ="99" GO TO LEO-DATOS.
-           IF ST-FILE > "07"
-               DISPLAY "error leyendo el archivo".
+       MUESTRO-PANTALLA.
+           DISPLAY " "                   LINE 1  COL 1 ERASE EOS
+                   "A.B.M. Clientes"     LINE 3  COL 32
+                   GUIONES               LINE 4  COL 1
+                   "Id Cliente : "       LINE 07 COL 10
+                   "01. Nombre     : "   LINE 10 COL 10
+                   "02. Direccion  : "   LINE 12 COL 10
+                   "03. Cod.Postal : "   LINE 14 COL 10
+                   "04. Categoria  : "   LINE 16 COL 10
+                   "Opcion [  ]"         LINE 20 COL 30
+                   GUIONES               LINE 22 COL 1.
+
+
+       INGRESO-ID.
+           ACCEPT W-CLI-ID LINE 07 COL 23.
+           MOVE   W-CLI-ID TO W-CLI-ID-Z.
+           DISPLAY W-CLI-ID-Z LINE 07 COL 23.
+           IF W-CLI-ID = 0 GO TO INGRESO-ID.
+
+       F-INGRESO-ID.
+           EXIT.
+
+       LEO-CLIENTES.
+           DISPLAY SPACES LINE 23 COL 1 SIZE 80.
+           MOVE W-CLI-ID TO CLI_ID.
+           READ CLIENTES INVALID KEY MOVE "N" TO EXISTE.
+           IF ST-FILE = "99" GO TO LEO-CLIENTES.
+           IF ST-FILE > "07" AND ST-FILE NOT = "23"
+                 STRING "Error leyendo Clientes Status = " ST-FILE
+                 DELIMITED BY SIZE INTO MENSAJE
+                 DISPLAY MENSAJE LINE 23 COL 1
+                 MOVE 1   TO HUBO-ERROR
+                 MOVE "N" TO EXISTE.
+
+       F-LEO-CLIENTES.
+           EXIT.
 
        MUESTRO-DATOS.
-           MOVE CLI_SALDO TO SALDO-Z.
-           DISPLAY "Id cliente"    LINE 10 COL 5.
-           DISPLAY "Saldo"         LINE 11 COL 5.
-           DISPLAY "Nombre"        LINE 12 COL 5.
-           DISPLAY "Direccion"     LINE 13 COL 5.
-           DISPLAY CLI_ID          LINE 10 COL 30.
-           DISPLAY  SALDO-Z        LINE 11 COL 30.
-           DISPLAY CLI_NOMBRE      LINE 12 COL 30.
-           DISPLAY CLI_DIRECCION   LINE 13 COL 30.
-           ACCEPT X LINE 14 COL 70.
 
+           IF EXISTE = "S"
+              MOVE CLI_NOMBRE    TO W-CLI-NOMBRE
+              MOVE CLI_DIRECCION TO W-CLI-DIRECCION
+              MOVE CLI_CODPOST   TO W-CLI-CODPOSTAL
+              MOVE CLI_CATEGORIA TO W-CLI-CATEGORIA.
+           DISPLAY CLI_NOMBRE    LINE 10 COL 36
+                   CLI_DIRECCION LINE 12 COL 36
+                   CLI_CODPOST   LINE 14 COL 36
+                   CLI_CATEGORIA LINE 16 COL 36.
 
-       F-LEO-DATOS.
+       CARGO-DATOS.
+           INITIALIZE DATOS.
+
+       INGRESO-NOMBRE.
+           MOVE W-CLI-NOMBRE TO W-CLI-NOMBRE-ANT.
+           ACCEPT W-CLI-NOMBRE LINE 10 COL 36.
+           IF W-CLI-NOMBRE = SPACES
+              MOVE W-CLI-NOMBRE-ANT TO W-CLI-NOMBRE
+              GO TO INGRESO-NOMBRE.
+           DISPLAY W-CLI-NOMBRE LINE 10 COL 36.
+
+       INGRESO-DIRECCION.
+           ACCEPT W-CLI-DIRECCION LINE 12 COL 36.
+           IF W-CLI-DIRECCION = SPACES
+              GO TO INGRESO-DIRECCION.
+           DISPLAY W-CLI-DIRECCION LINE 12 COL 36.
+
+       INGRESO-CODPOSTAL.
+           ACCEPT W-CLI-CODPOSTAL LINE 14 COL 36.
+           IF W-CLI-CODPOSTAL = SPACES
+              GO TO INGRESO-CODPOSTAL.
+           DISPLAY W-CLI-CODPOSTAL LINE 14 COL 36.
+
+       INGRESO-CATEGORIA.
+           ACCEPT W-CLI-CATEGORIA LINE 16 COL 36.
+           IF W-CLI-CATEGORIA = SPACES
+              GO TO INGRESO-CATEGORIA.
+           DISPLAY W-CLI-CATEGORIA LINE 16 COL 36.
+
+       F-CARGO-DATOS.
            EXIT.
-       END PROGRAM INICIO.
+
+       OPCIONES.
+           DISPLAY "[00 - ENTER] GRABAR" LINE 23 COL 1
+                   "[88] - BORRAR"       LINE 24 COL 1
+                   "[77] - SALIR"        LINE 23 COL 20.
+           ACCEPT OPCION LINE 20 COL 38.
+           IF FIN = "N"
+              EVALUATE OPCION
+                WHEN 1
+                   PERFORM INGRESO-NOMBRE
+                WHEN 2
+                   PERFORM INGRESO-DIRECCION
+                WHEN 3
+                   PERFORM INGRESO-CODPOSTAL
+                WHEN 4
+                   PERFORM INGRESO-CATEGORIA
+                WHEN 0
+                   PERFORM GRABAR THRU F-GRABAR
+                WHEN 88
+                   PERFORM BORRAR
+                WHEN 77
+                   MOVE "S" TO FIN
+                WHEN OTHER
+                   GO TO OPCIONES
+              END-EVALUATE
+              IF OPCION > 0 AND OPCION < 77 GO TO OPCIONES.
+
+       GRABAR.
+           MOVE W-CLI-NOMBRE    TO CLI_NOMBRE CLI_NOMBRE_2.
+           MOVE W-CLI-DIRECCION TO CLI_DIRECCION.
+           MOVE W-CLI-CODPOSTAL TO CLI_CODPOST.
+           MOVE W-CLI-CATEGORIA TO CLI_CATEGORIA CLI_CATEGORIA_2.
+
+       GRABO.
+           IF EXISTE = "S" GO TO REGRABO.
+           WRITE REG-CLIENTES.
+           IF ST-FILE = "99" GO TO GRABO.
+           IF ST-FILE > "07"
+              STRING "Error al GRABAR Clientes " ST-FILE
+                 DELIMITED BY SIZE INTO MENSAJE
+              DISPLAY MENSAJE LINE 24 COL 40
+              ACCEPT X LINE 23 COL 77.
+           GO TO F-GRABAR.
+
+       REGRABO.
+           REWRITE REG-CLIENTES.
+           IF ST-FILE = "99" GO TO REGRABO.
+           IF ST-FILE > "07"
+              STRING "Error al reGRABAR Clientes " ST-FILE
+                 DELIMITED BY SIZE INTO MENSAJE
+              DISPLAY MENSAJE LINE 24 COL 40
+              ACCEPT X LINE 23 COL 77.
+
+       F-GRABAR.
+           EXIT.
+
+       BORRAR.
+           DELETE CLIENTES.
+           IF ST-FILE = "99" GO TO BORRAR.
+           IF ST-FILE > "07"
+              STRING "Error al borrar Clientes " ST-FILE
+                 DELIMITED BY SIZE INTO MENSAJE
+              DISPLAY MENSAJE LINE 24 COL 40.
+
+
+       END PROGRAM "INICIO".
